a1831de08b40603e7ec65184b74a11d30b266f58
thiagonmiziara <thiagonmiziara@gmail.com>
2020-11-14 16:15:23 -0300

    add mais um projeto

---
+++ js/script.js
@@ -0,0 +1,23 @@
+import Countdown from './countdown.js';
+const natal = document.querySelector('.natal');
+const novo = document.querySelector('.novo');
+
+const tempoParaONatal = new Countdown('24 December 2020 23:59:59 GMT-0300');
+const tempoParaOAnoNovo = new Countdown('31 December 2020 23:59:59 GMT-0300');
+
+
+setInterval(() => {
+    natal.innerHTML = `
+         <div class="dias"> ${tempoParaONatal.total.days} Dias</div>
+        <div class="horas"> ${tempoParaONatal.total.hours} Horas</div>
+        <div class="minutos"> ${tempoParaONatal.total.minutes} Minutos</div>
+        <div class="segundos"> ${tempoParaONatal.total.seconds} Segundos</div>`;
+}, 1000)
+
+setInterval(() => {
+    novo.innerHTML = `
+         <div class="dias"> ${tempoParaOAnoNovo.total.days} Dias</div>
+        <div class="horas"> ${tempoParaOAnoNovo.total.hours} Horas</div>
+        <div class="minutos"> ${tempoParaOAnoNovo.total.minutes} Minutos</div>
+        <div class="segundos"> ${tempoParaOAnoNovo.total.seconds} Segundos</div>`;
+}, 1000)
