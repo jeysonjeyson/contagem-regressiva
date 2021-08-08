99c92c8177d9ebad45910464c7ddfbc3d079d498
jeysonjeyson <88634101+jeysonjeyson@users.noreply.github.com>
2021-08-08 18:47:56 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ import Countdown from './countdown.js';
 const natal = document.querySelector('.natal');
 const novo = document.querySelector('.novo');
 
-const tempoParaONatal = new Countdown('24 December 2020 23:59:59 GMT-0300');
+const tempoParaONatal = new Countdown('04 February 2022 23:59:59 GMT-0300');
 const tempoParaOAnoNovo = new Countdown('31 December 2020 23:59:59 GMT-0300');
 
 
